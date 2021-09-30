4faf416abb642dea1392b16928be69820c917be5
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-09-30 12:45:31 -0400

    create two new tables for assignments

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,16 @@
+CREATE TABLE assignments(id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(255) NOT NULL,
+content TEXT,
+day INTEGER,
+chapter INTEGER,
+duration INTEGER
+);
+-- SELECT * FROM assignments;
+
+CREATE TABLE assignment_submissions(id SERIAL PRIMARY KEY NOT NULL,
+student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+duration INTEGER,
+submission_date DATE
+);
+SELECT count(*) FROM assignment_submissions;
